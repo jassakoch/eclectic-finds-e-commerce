db9620321292c8af184a03daa0eda525fa2b8d72
paulina koch <pjassakoch@gmail.com>
2024-01-09 21:18:41 -0500

    Fixed relationship association in products-routes.js file to correctly reference relationships between products, tag and category

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,19 +4,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.status(200).json(productData)
+  }
+  catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // Finding a single product by its `id`
+  // Including its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+
+    if (productData===null) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/',  async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,6 +50,8 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ 
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
