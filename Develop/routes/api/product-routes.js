d7960e710dd7ec4a995182f70ddf0363ac529721
paulina koch <pjassakoch@gmail.com>
2024-01-10 09:56:57 -0500

    Fixed last bug with product-routes.js and post

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -40,34 +40,39 @@ router.get('/:id', async (req, res) => {
 
 });
 
-// create new product
-router.post('/', async (req, res) => {
 
-  try {
-    const productData = await Product.create({
-      product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      tagIds: req.body.tagIds,
-    });
-  
+
+router.post('/', (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+  Product.create(req.body)
+    .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-    const productTagIdArr = req.body.tagIds.map((tag_id) => {
-      return {
-        product_id: productData.id,
-        tag_id,
-      };
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
-    return ProductTag.bulkCreate(productTagIdArr);
-  }
-  // if no product tags, just respond
-  res.status(200).json(productData);
-
-} catch (error) {
- res.status(400).json(error);
-}
 });
+
 
 // update product
 router.put('/:id', (req, res) => {
